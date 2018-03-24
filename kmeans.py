0359f6797aec11413ee94e600450906bdd9376c1
Akshit Meghawat <meghawat.akshit@gmail.com>
2018-03-24 13:22:20 -0400

    k means on a neighborhood image

---
+++ kmeans.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('ilk-3b-1024.tif')
+Z = img.reshape((-1, 3))
+
+# convert to np.float32
+Z = np.float32(Z)
+
+# define criteria, number of clusters(K) and apply kmeans()
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 5
+
+for K in range(1, 6):
+    ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+
+    cv2.imwrite('res2' + '.jpg', res2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
